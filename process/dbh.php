0fc02f817a945e0927a35628d0835de86226bc71
gpodder770 <71787759+gpodder770@users.noreply.github.com>
2025-04-10 15:39:56 +0600

    teke action to run the site form live to localhsot

--- process/dbh.php
+++ process/dbh.php
@@ -1,16 +1,38 @@
 <?php
 	date_default_timezone_set("Asia/Dhaka");
-	$servername = "localhost";
-    $dBUsername = "yourtec1_alms_usr";
-    $dbPassword = "{]PzQ%6~yHF,";
-    $dBName = "yourtec1_alms_v1";
+
+	// $environment = 'production';
+	$environment = 'localhost';
+
+	if( $environment == 'production' ) {
+		$servername = "localhost";
+		$dBUsername = "yourtec1_alms_usr";
+		$dbPassword = "{]PzQ%6~yHF,";
+		$dBName = "yourtec1_alms_v1";
+	} elseif ( $environment == 'localhost' ) {
+		$servername = "localhost";
+		$dBUsername = "root";
+		$dbPassword = "";
+		$dBName = "alms";
+	}
+
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=".$dBName, $dBUsername, $dbPassword);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (\Exception $e) {
 		die();
 	}
-	$baseurl = "https://alms.yourtechsolution.in/";
+
+
+	
+	if ( $environment == 'production' ) {
+		$baseurl = "https://alms.yourtechsolution.in/";
+	} elseif ( $environment == 'localhost' ) {
+		$baseurl = "http://localhost/alms";
+	}
+	
+
+
 	function generateSanitize($input, $type = 'string'){
 		$typeList = [
 			'string' => FILTER_SANITIZE_STRING,
